a93f090c45eba1cefb5392f923737b6cb07e4936
AntoM37 <marchioroantonella@gmail.com>
2023-08-31 16:46:56 -0300

    Manejador de eventos - div

--- eventos.js
+++ eventos.js
@@ -1,1 +1,9 @@
-alert 
+const divElement = document.querySelector('#Saludo');
+
+divElement.addEventListener('click', function() {
+    alert('Hola! Soy el div');
+});
+
+function myFunction() {
+    alert("Hola!");
+}
